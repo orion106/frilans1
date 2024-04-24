416e42bd660da179f95d3655c2d804aafee0be5b
orion106 <mr.KATYAS@mail.ru>
2024-04-24 21:37:19 +0300

    route commit

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route::get('/q', function () {
 });
 Route::middleware('Customer')->group(function () {
     Route::get('profile/order', [OrderController::class, 'create'])->name('order');
-
     Route::post('profile/order', [OrderController::class, 'create']);
 });
 require __DIR__.'/auth.php';
