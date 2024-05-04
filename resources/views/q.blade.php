be569885d74e2382bca903496f276c4f31b7ce21
FasTIK-star <danya.miller.99@bk.ru>
2024-05-04 22:08:28 +0700

    employers

--- resources/views/q.blade.php
+++ resources/views/q.blade.php
@@ -1,1 +1,34 @@
 <x-layout.navigation></x-layout.navigation>
+<div class="wrapper-title">
+    <div class="title">
+    <h1 class="question">С чего начать<br> поиск исполнителя?</h1>
+        <div class="explanation_blocks">
+            <div class="block-1-1">
+                <div class="numder"><h2>01</h2></div><hr>
+                <div class="title-block"><h5>Регистрация</h5></div>
+                <div>Создайте аккаунт</div>
+            </div>
+            <div class="block-2-2">
+                <div class="numder"><h2>02</h2></div><hr>
+                <div class="title-block"><h5>Создание заказа</h5></div>
+                <div class="description-block">Создайте заказ</div>
+            </div>
+            <div class="block-3-3">
+                <div class="numder"><h2>03</h2></div><hr>
+                <div class="title-block"><h5>Выбор исполнителя</h5></div>
+                <div class="description-block">Выберите кандидата из<br> списка откликнувшихся</div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="title-2">
+    <div class="btn-create-wrapper">
+    <h1 class="question2">Разместите<br> свое задание здесь</h1>
+        <div class="btn-123">
+            <button class="btn-create">
+                <b>Создать задание</b>
+            </button>
+        </div>
+    </div>
+</div>
+
